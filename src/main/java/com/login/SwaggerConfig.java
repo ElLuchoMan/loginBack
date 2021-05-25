fb52759e364fd5907aeace8bf973363e75169d45
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-05-25 15:35:03 -0500

    Agregado Swagger

--- src/main/java/com/login/SwaggerConfig.java
+++ src/main/java/com/login/SwaggerConfig.java
@@ -1,12 +1,10 @@
 package com.login;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,24 +12,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-	
 	@Bean
-	public Docket swaConfig()
-	{
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.paths(PathSelectors.any())
-				.apis(RequestHandlerSelectors.basePackage("com.dan.ejer.controller"))
-				.build().apiInfo(getApiInfo());
+	public Docket api() {
+		 return new Docket(DocumentationType.SWAGGER_2).select()
+	                .apis(RequestHandlerSelectors
+	                        .basePackage("com.login.controller"))
+	                .paths(PathSelectors.any())
+	                .build();
 	}
 
-	private ApiInfo getApiInfo() {
-		return new ApiInfoBuilder().title("TITULO PROYECTO - EMPLEADO")
-				.description("Primera implemetacion de Swagger")
-				.contact(new Contact("Daniel U", ".html", "@hotmail.com"))
-				.license("Unica")
-				.licenseUrl("urlLicencia")
-				.version("1.0.0")
-				.build();
-	}
 }
--- src/main/java/com/login/security/WebSecurity.java
+++ src/main/java/com/login/security/WebSecurity.java
@@ -1,6 +1,5 @@
 package com.login.security;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,58 +13,60 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 import com.login.security.JWT.JwtEntryPoint;
 import com.login.security.JWT.JwtTokenFilter;
 import com.login.service.UserDetailsServiceImpl;
+
+import io.swagger.models.HttpMethod;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    UserDetailsServiceImpl userDetailsServiceImpl;
+	@Autowired
+	UserDetailsServiceImpl userDetailsServiceImpl;
 
-    @Autowired
-    JwtEntryPoint jwtEntryPoint;
+	@Autowired
+	JwtEntryPoint jwtEntryPoint;
 
-    @Bean
-    public JwtTokenFilter jwtTokenFilter(){
-        return new JwtTokenFilter();
-    }
+	@Bean
+	public JwtTokenFilter jwtTokenFilter() {
+		return new JwtTokenFilter();
+	}
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 
-    @Override
-    protected AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManager();
-    }
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
-    }
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/api/auth/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and().authorizeRequests().antMatchers("/api/auth/**").permitAll().antMatchers("/api/test/**")
+				.permitAll() // permit the class of test
+				.antMatchers("/**").permitAll() // permit all the routers after swagger-ui.html
+				.anyRequest().authenticated();
+		http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
+
 }
