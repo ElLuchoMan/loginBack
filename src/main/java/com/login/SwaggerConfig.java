76165e87bcdc2513d3239a52793650c018fb8354
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-05-25 16:03:06 -0500

    Finalizado

--- src/main/java/com/login/SwaggerConfig.java
+++ src/main/java/com/login/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.login;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,5 +23,14 @@ public class SwaggerConfig {
 	                .paths(PathSelectors.any())
 	                .build();
 	}
-
+	@SuppressWarnings("unused")
+	private ApiInfo getApiInfo() {
+		return new ApiInfoBuilder().title("TITULO PROYECTO - EMPLEADO")
+				.description("Primera implemetacion de Swagger")
+				.contact(new Contact("baluisto96", ".html", "@gmail.com"))
+				.license("Unica")
+				.licenseUrl("urlLicencia")
+				.version("1")
+				.build();
+	}
 }
