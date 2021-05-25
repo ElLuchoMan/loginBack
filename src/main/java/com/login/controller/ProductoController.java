dd5b7fc40adb4afb48306d9961395e3c17e401fc
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-05-25 09:39:41 -0500

    Agregado cors

--- src/main/java/com/login/controller/ProductoController.java
+++ src/main/java/com/login/controller/ProductoController.java
@@ -6,6 +6,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.login.service.ProductoService;
 
 @RestController
 @RequestMapping("/api/productos")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductoController {
 
     @Autowired
