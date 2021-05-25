c9914970aa7f8e2630db3bbca1ffda668a6dd4c8
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-05-25 11:31:06 -0500

    Creado rolService

---
+++ src/main/java/com/login/service/RolService.java
@@ -0,0 +1,23 @@
+package com.login.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.login.entity.Rol;
+import com.login.repository.RolRepository;
+
+import enums.RolNombre;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+public class RolService {
+
+    @Autowired
+    RolRepository rolRepository;
+
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return rolRepository.findByRolNombre(rolNombre);
+    }
+}
