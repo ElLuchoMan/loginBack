93bfbcd4ba2b4b2d92fdc1cad7fbfdb39a102081
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-05-25 12:50:25 -0500

    Agregado CORS

--- src/main/java/com/login/controller/AuthController.java
+++ src/main/java/com/login/controller/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +34,7 @@ import java.util.Set;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     @Autowired
