479ea438c607a6adddb53b91eb0e25fee3566c38
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-05-25 11:50:57 -0500

    Se crea UserServiceImpl

---
+++ src/main/java/com/login/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.login.service;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.login.entity.Usuario;
+import com.login.security.UsuarioPrincipal;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String nombreUsuario) throws UsernameNotFoundException {
+        Usuario usuario = usuarioService.getByNombreUsuario(nombreUsuario).get();
+        return UsuarioPrincipal.build(usuario);
+    }
+}
