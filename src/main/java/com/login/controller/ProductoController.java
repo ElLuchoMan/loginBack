7e73b460b230ca636053ef5d1cc37a5d4987c382
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-05-25 12:09:24 -0500

    Fixed ProductoController

--- src/main/java/com/login/controller/ProductoController.java
+++ src/main/java/com/login/controller/ProductoController.java
@@ -6,23 +6,18 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import com.login.DTO.Mensaje;
 import com.login.entity.Producto;
 import com.login.service.ProductoService;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/productos")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "http://localhost:4200&quot;)
 public class ProductoController {
 
     @Autowired
@@ -43,6 +38,7 @@ public class ProductoController {
     }
 
     @PostMapping("nuevo")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> create(@RequestBody Producto producto){
         if(StringUtils.isBlank(producto.getNombreProducto()))
             return new ResponseEntity(new Mensaje("el nombre es obligatorio"), HttpStatus.BAD_REQUEST);
@@ -55,6 +51,7 @@ public class ProductoController {
     }
 
     @PutMapping("/actualizar/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> update(@RequestBody Producto producto, @PathVariable("id") Long id){
         if(!productoService.existePorId(id))
             return new ResponseEntity(new Mensaje("no existe ese producto"), HttpStatus.NOT_FOUND);
@@ -73,6 +70,7 @@ public class ProductoController {
     }
 
     @DeleteMapping("/borrar/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> delete(@PathVariable Long id){
         if(!productoService.existePorId(id))
             return new ResponseEntity(new Mensaje("no existe ese producto"), HttpStatus.NOT_FOUND);
